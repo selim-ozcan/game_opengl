137b76293b14e09278cd51eeccb918fc6812a8e5
selimozcan <selimozcan17@gmail.com>
2024-02-24 18:30:20 +0300

    Initial Commit

---
+++ game_opengl/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  game_opengl
+//
+//  Created by Selim Özcan on 24.02.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
